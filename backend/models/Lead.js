83cd3fffd16bc11771b0bfba60f291dce19da777
fifi56336101-commits <fifi56336101@gmail.com>
2026-07-06 12:29:55 +0000

    update

--- backend/models/Lead.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LeadSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Lead', LeadSchema);
